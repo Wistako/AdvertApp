15e3fd5c3a03e09790b0c967e59e5c3c6dbbb07f
Piotr Słomczyński <wistako@gmail.com>
2024-04-05 17:10:30 +0200

    Build app

--- db.js
+++ db.js
@@ -2,8 +2,12 @@ const mongoose = require('mongoose');
 
 const connectToDB = () => {
 
+  const dbURI = process.env.NODE_ENV === 'production'
+  ? `mongodb+srv://Wistako:${process.env.DB_PASS}@cluster0.80soovx.mongodb.net/AdvertDB?retryWrites=true&w=majority&appName=Cluster0`
+  : 'mongodb://localhost:27017/AdvetrDB';
+
   // connect to the database
-  mongoose.connect('mongodb://localhost:27017/AdvertDB', { useNewUrlParser: true, useUnifiedTopology: true });
+  mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
   const db = mongoose.connection;
 
   // on succes
